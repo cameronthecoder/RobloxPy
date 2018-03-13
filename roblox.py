a535648f7ec1472ace690ed877cfee46e6d2df66
Cameron <camnooten@mail.com>
2018-03-13 08:54:22 -0500

    coming soon

---
+++ roblox.py
@@ -0,0 +1,44 @@
+import requests
+class User:
+    def friends(username):
+        print()
+    def places(username):
+        print()
+    def badges(username):
+        print()
+    def robux(username):
+        print()
+class Group:
+    def description(username):
+        print()
+    def name(id):
+        print()
+class Asset:
+    def asset_id(id):
+        print()
+    def product_id(id):
+        print()
+    def name(id):
+	print()
+    def description(id):
+        print()
+    def asset_type_id(id):
+        print()
+    def created_at(id):
+        print()
+    def last_updated(id):
+        print()
+    def price_in_robux(id):
+        print()
+    def price_in_tickets(id):
+        print()
+    def sales(id):
+        print()
+    def is_new(id):
+        print()
+    def is_for_sale(id):
+        print()
+    def is_limited(id):
+        print()
+    def remaining(id):
+        print()
