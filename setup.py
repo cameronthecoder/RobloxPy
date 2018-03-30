1fc51589a16afa8fa64c72a6a5ba803bf0ea95e8
Cameron Dahl <camnooten@mail.com>
2018-03-30 09:37:35 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(name='robloxpy',
+      version='0.1',
+      description='A ROBLOX API wrapper written in Python 3.',
+      url='https://github.com/camnooten/robloxpy',
+      author='Cameron',
+      author_email='camnooten@gmail.com',
+      license='MIT',
+      packages=['src'],
+      install_requires=['requests'],
+      zip_safe=False)
